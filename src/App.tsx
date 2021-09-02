bc498557f64a297c80c851705b96817f4afab0ba
Billy Kaufman <billyhkaufman@gmail.com>
2021-09-02 16:03:58 -0400

    Added svg headshot

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,41 @@ import { useApplicationContext } from './state/contexts/ApplicationContext';
 import { ILink } from './interfaces/link';
 import RouteHandler from './components/Route/RouteHandler';
 import { MainNav } from './components/Nav';
+import { motion } from 'framer-motion';
 
 const App: React.FC<RouteComponentProps> = () => {
-  useScrollToTopOnNav();
+	useScrollToTopOnNav();
 
-  const {
-    state: { links }
-  } = useApplicationContext()
+	const {
+		state: { links },
+	} = useApplicationContext();
 
-  return (
-    <div className="site-container">
-      {/* className="site-container" */}
-      {/* <MainNav /> */}
-      {/* <div className="App-Loader"></div> */}
-      <Switch>
-        {links.map((l: ILink) => {
-          return (
-            <RouteHandler
-              key={l.id}
-              path={l.to}
-              exact
-              component={l.component}
-            />
-          );
-        })}
-      </Switch>
-    </div>
-  );
-}
+	return (
+		<motion.div
+			className='site-container'
+			initial={{
+				opacity: 0,
+			}}
+			animate={{
+				opacity: 1,
+			}}>
+			{/* className="site-container" */}
+			{/* <MainNav /> */}
+			{/* <div className="App-Loader"></div> */}
+			<Switch>
+				{links.map((l: ILink) => {
+					return (
+						<RouteHandler
+							key={l.id}
+							path={l.to}
+							exact
+							component={l.component}
+						/>
+					);
+				})}
+			</Switch>
+		</motion.div>
+	);
+};
 
 export default withRouter(App);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,112 @@
-import "./styles/Home.scss";
-import headshot from "../assets/images/ton headshot- face only lo res.png";
-import { Imporivisation1 } from "../components/SVG/Imporivisation1";
-import { WoodsyScene1 } from "../components/SVG/WoodsyScene1";
-import { Improvisation } from "../components/SVG/Improvisation";
+import './styles/Home.scss';
+// import headshot from '../assets/images/ton headshot- face only lo res.png';
+// import { Imporivisation1 } from '../components/SVG/Imporivisation1';
+import { WoodsyScene1 } from '../components/SVG/WoodsyScene1';
+import { Improvisation } from '../components/SVG/Improvisation';
 // import { Whalesback } from "../components/SVG/Whalesback";
+import { motion } from 'framer-motion';
+import { myAvatar as MyAvatar } from '../components/SVG/myAvatar';
 
 const Home = () => {
-  return (
-    <>
-      <div className="home-content">
-        <div className="background-svg">
-          <WoodsyScene1 />
-        </div>
-        {/* <div className="home-content-text"> */}
-          {/* HOME CONTENT test test */}
-          {/* <Improvisation /> */}
-        {/* </div> */}
-      </div>
-    </>
-  )
-}
+	const headshotVariants = {
+		hidden: {
+			// opacity: 0,
+			x: '-100%',
+		},
+		visible: {
+			// opacity: 1,
+			x: '0',
+		},
+	};
+
+	const headshotTransition = {
+		default: {
+			duration: 0.25,
+			ease: 'easeIn',
+		},
+		// opacity: {
+		//   duration: 1,
+		// }
+	};
+
+	const titleVariants = {
+		hidden: {
+			opacity: 0,
+			x: '100%',
+		},
+		visible: {
+			opacity: 1,
+			x: '0',
+		},
+	};
+	const titleTransition = {
+		default: {
+			duration: 0.25,
+			ease: 'easeIn',
+		},
+		opacity: {
+			duration: 1,
+		},
+	};
+
+	return (
+		<>
+			<div className='home-content'>
+				<div className='background-svg'>
+					<WoodsyScene1 />
+				</div>
+				<div className='home-content-text'>
+					<motion.div
+						variants={headshotVariants}
+						transition={headshotTransition}
+						initial='hidden'
+						animate='visible'
+						className='headshot'>
+						{/* <Improvisation /> */}
+						<MyAvatar />
+					</motion.div>
+
+					<motion.div
+						variants={titleVariants}
+						transition={titleTransition}
+						initial='hidden'
+						animate='visible'
+						className='title'>
+						<Improvisation />
+					</motion.div>
+
+					{/* <motion.img
+						src={headshot}
+						variants={headshotVariants}
+						transition={headshotTransition}
+						initial='hidden'
+						animate='visible'
+						className='headshot'
+					/> */}
+				</div>
+
+				{/* <div className="justify-between w-full">
+          <motion.div className="home-content-text"
+            variants={headshotVariants}
+            transition={headshotTransition}
+            initial="hidden"
+            animate="visible"
+          >
+            <Improvisation />
+          </motion.div>
+
+          <motion.div className="home-content-text"
+            variants={headerVariants}
+            transition={headerTransition}
+            initial="hidden"
+            animate="visible"
+          >
+            <Improvisation />
+          </motion.div>
+        </div> */}
+			</div>
+		</>
+	);
+};
 
 export default Home;
